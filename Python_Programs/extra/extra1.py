d3ec0b38c336f8cd056d14052417981b59fe5827
Amogh Sandeep <amoghsandeep@gmail.com>
2022-08-29 09:54:29 -0400

    from silveroak

--- Python_Programs/extra/extra1.py
+++ Python_Programs/extra/extra1.py
@@ -1,9 +1,22 @@
 print("")
 a=5
+f=5.6
 b="ram"
 c=True
 l1=[11,22]
 d1={11:"ram",22:"nayan"}
 t1=(33,44)
 s1={11,22,33,44}
-print(type(a))
+print(type(a)) #static
+
+"""
+if ... else
+if elif else
+
++ - * / // **
+> <>= <= == != 
+and or not
+
+for 
+while (Wat is the different between while and for)
+"""
