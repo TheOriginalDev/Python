f76085bb32e5cb6360f4aa7549fd865481bb5ac8
Amogh Sandeep <amoghsandeep@gmail.com>
2022-08-30 17:36:43 -0400

    problem #5 in pythonex

---
+++ Python_Programs/extra/extra8_#5.py
@@ -0,0 +1,6 @@
+n1 = int(input("Enter Number #1 Value =>"))
+n2 = int(input("Enter Number #2 Value =>"))
+if n1 % n2 == 0:
+    print(n1, "is divisible by", n2)
+else:
+    print(n1, "is not divisible by", n2)
