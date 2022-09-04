50c308bdd4475f47e528d32c1e534284abd9a24b
Amogh Sandeep <amoghsandeep@gmail.com>
2022-09-03 20:30:03 -0400

    practicing while loops

---
+++ Python_Programs/extra/extra11.py
@@ -0,0 +1,10 @@
+limit = int(input("Enter limit =>"))
+sum=0
+number=0
+i = 1
+while i <= limit:
+    print(i)
+    sum=sum+i
+    i = i + 1
+
+print("The sum of all the numbers =>",sum)
---
+++ Python_Programs/extra/extra12.py
@@ -0,0 +1,10 @@
+limit=int(input("Enter limit =>"))
+even=0
+odd=0
+i=1
+while i<=limit:
+    if i%2==0:
+        even=even+1
+    else:
+        odd=odd+1
+print("The amount of even numbers =>", even,"The amount of odd numbers =>",odd)
